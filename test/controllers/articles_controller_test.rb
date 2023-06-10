8ee2c1623c8960675597a4fed2d56e9b1708acdb
SashaSub <ssuboch@gMAIL.com>
2023-06-09 23:49:52 -0400

    first commit

---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+  
+  test "should get new" do
+    sign_in users(:one)
+    get new_article_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    sign_in users(:one) 
+    assert_difference("Article.count") do 
+      post articles_url, params: { article: { body: @article.body, title: @article.title } } 
+    end     
+      
+    assert_redirected_to article_url(Article.last)
+  end
+
+  test "should show article" do
+    get article_url(@article)
+    assert_response :success
+  end
+
+  test "should get edit" do    
+    sign_in users(:one)      
+    get edit_article_url(@article)
+    assert_response :success
+  end
+
+  test "should update article" do    
+    sign_in users(:one)   
+    patch article_url(@article), params: { article: { body: @article.body, title: @article.title } }
+    assert_redirected_to article_url(@article)
+  end
+
+  test "should destroy article" do
+    sign_in users(:one)    
+    assert_difference("Article.count", -1) do
+      delete article_url(@article)
+    end
+
+    assert_redirected_to articles_url
+  end
+end
